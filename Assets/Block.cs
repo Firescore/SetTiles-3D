bb06f9d2d73d18444e6f46456b505faa6e6511d2
Harry <63226772+harry3973@users.noreply.github.com>
2020-07-15 10:55:57 +0530

    v1
    CTR

---
+++ Assets/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+	
+	//	public GameObject pair;
+	public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    mat = gameObject.GetComponent<MeshRenderer>().material;
+	    Color col = mat.color;
+	    col.a = 0.0f;
+	    mat.color = col;
+	    gameObject.GetComponent<MeshRenderer>().material = mat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	public void CheckPair(GameObject obj)
+	{
+		
+		if(obj.name == gameObject.name)
+		{
+			obj.SetActive(false);
+			mat = gameObject.GetComponent<MeshRenderer>().material;
+			Color col = mat.color;
+			col.a = 1f;
+			mat.color = col;
+			gameObject.GetComponent<MeshRenderer>().material = mat;
+			gameObject.name = "DONE";
+		}
+	}
+}
---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{   
+	public GameObject target,dragObjTarget,current_obj;
+	public 	bool isMouseDragging;
+	Vector3 screenPosition,offset;
+	Vector3 targetInitialPos;
+	public LayerMask mask;
+
+	
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+		
+			RaycastHit hitInfo;
+			target = ReturnClickedObject(out hitInfo);
+			if (target != null)
+			{
+				isMouseDragging = true;
+			
+				screenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
+				offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			
+			isMouseDragging = false;
+	        
+			if(target != null)
+			{
+				Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+				Vector3 MousePos = Camera.main.ScreenToWorldPoint (currentScreenSpace);
+				target.transform.position = targetInitialPos;
+				target.layer = 8;
+				target = null;
+			}
+		
+		}
+		RaycastHit hits;
+		if (isMouseDragging)
+		{
+			if (target  == null)
+			{
+				return;
+			}
+			//tracking mouse position.
+			Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+
+			Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) ;
+			target.transform.position = Vector3.MoveTowards(target.transform.position,currentPosition,10);
+			
+			
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray.origin, ray.direction * 10, out hits,Mathf.Infinity , mask))
+			{
+				current_obj = hits.collider.gameObject;
+			
+				if(target !=  null)
+				{
+					
+					if(	hits.collider.gameObject.name == target.gameObject.name)
+					{
+						hits.collider.gameObject.GetComponent<Block>().CheckPair(target.gameObject);
+						isMouseDragging = false;
+					}
+					
+				
+				}
+		
+			}
+		}
+	
+	
+	}
+	int found;
+	GameObject ReturnClickedObject(out RaycastHit hit)
+	{
+		GameObject targetObject = null;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray.origin, ray.direction * 10, out hit,Mathf.Infinity , mask))
+		{
+			current_obj = hit.collider.gameObject;
+			
+			if(target !=  null)
+			{
+				hit.collider.gameObject.GetComponent<Block>().CheckPair(target.gameObject);
+				return null;;
+			}
+			
+		
+			targetObject = hit.collider.gameObject;
+			targetInitialPos = targetObject.transform.position;
+			targetObject.layer = 0;
+		
+            
+		   
+		}
+		return targetObject.gameObject;
+	}
+    
+    
+
+}
