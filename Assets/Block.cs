ec93273fe9ed48e26bcae2fec84db1541be615f5
Harry <63226772+harry3973@users.noreply.github.com>
2020-07-24 19:40:17 +0530

    CTR Art initgration
    Art initgration

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -7,6 +7,8 @@ public class Block : MonoBehaviour
 	
 	//	public GameObject pair;
 	public Material mat;
+	public LevelComplete levlecompelete;
+	//	public Material final_col;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,12 +31,18 @@ public class Block : MonoBehaviour
 		if(obj.name == gameObject.name)
 		{
 			obj.SetActive(false);
-			mat = gameObject.GetComponent<MeshRenderer>().material;
-			Color col = mat.color;
-			col.a = 1f;
-			mat.color = col;
-			gameObject.GetComponent<MeshRenderer>().material = mat;
+		
+			//	mat = gameObject.GetComponent<MeshRenderer>().material;
+			//	Color col = mat.color;
+			//	col.a = 1f;
+			//	mat.color = col;
+			//gameObject.GetComponent<MeshRenderer>().material = mat;
+			gameObject.layer = 0;
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
 			gameObject.name = "DONE";
+		
+			Destroy(obj);
+			levlecompelete.CheckONLevelComplete();
 		}
 	}
 }
---
+++ Assets/LevelComplete.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+	public GameObject[] tiles;
+	public Color col;
+	public GameObject white_img;
+	public GameObject green_tiles,parent_tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	public void CheckONLevelComplete()
+	{
+		int j  = gameObject.transform.childCount;
+		Debug.Log(j);
+		if( j == 1)
+		{
+			parent_tiles.SetActive(true);
+			green_tiles.SetActive(true);
+			
+			//for(int i = 0; i <tiles.Length;i++)
+			//{
+			//	tiles[i].GetComponent<MeshRenderer>().material.color = col;
+			//}
+			
+			StartCoroutine("Effect");
+		}
+	}
+	
+	IEnumerator Effect()
+	{
+		for(int i =0; i<4;i++)
+		{
+			GameObject temp_obj = Instantiate(white_img,white_img.transform.position,white_img.transform.rotation);
+			
+			temp_obj.SetActive(true);
+			
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+}
